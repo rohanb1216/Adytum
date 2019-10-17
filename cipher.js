b3d4359c29af5a28af73f5142890d0a511352e6a
rohanb1216 <rohan.biju@gmail.com>
2019-10-17 20:16:45 +0530

    login

--- cipher.js
+++ cipher.js
@@ -1,14 +1,34 @@
-function cipher(text){
+var question = "name";
+
+function cipher(text) {
     var len = text.length;
-    for(var i = 0; i<len;i++){
-        text.replace(text.charAt(i)+3); 
+    for (var i = 0; i < len; i++) {
+        text.replace(text.charAt(i) + 3);
     }
 }
 
-function caesarCipher(str, key){
-    return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0)-65 + key ) % 26 + 65));
-  }
-
-function showText(text){
-    document.getElementById("cipherText").innerHTML=text;
+function caesarCipher(str, key) {
+    return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65));
 }
+
+function showText(text) {
+    document.getElementById("question").innerHTML = text;
+}
+
+function check() {
+    var answer = document.getElementById("answer").value;
+    if (answer == question) {
+        solved(room1);
+    }
+    else {
+        document.getElementById("answer").classList.add("incorrect");
+    }
+}
+
+window.onload(function () {
+
+    var text = caesarCipher(question, Math.floor(Math.random() * 26));
+    showText(text);
+    document.getElementsByTagName("button")[0].addEventListener("click", check());
+
+})
