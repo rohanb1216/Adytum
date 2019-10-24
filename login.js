e1f1235fea140795bd8c8b6242c749bee1e7a0bc
rohanb1216 <rohan.biju@gmail.com>
2019-10-24 20:06:07 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function login(){
     //placeholder for actual login
     //pass values to server, go to the room, login token should be passed to keep login
-    var login=true;//true if login successful
+    var login;//true if login successful
     var uname = document.getElementById("username").value;
     var pass =  document.getElementById("password").value;
     if(login){
