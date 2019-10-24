b804f79c0a886af4fd99e0778a1a11876c02780a
rohanb1216 <rohan.biju@gmail.com>
2019-10-24 19:48:03 +0530

    login

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
 function login(){
     //placeholder for actual login
     //pass values to server, go to the room, login token should be passed to keep login
+    var login=true;//true if login successful
     var uname = document.getElementById("username").value;
     var pass =  document.getElementById("password").value;
-    window.location.href="room.html";
+    if(login){
+        window.location.href="room.html";
+    }
 } 
