4f29cd4abf3819c6f1a57671f258ef55623a5a93
Abhishek262 <abhishek262014@gmail.com>
2019-11-10 17:44:17 +0530

    Modified login and register php files

--- cgi-bin/login.php
+++ cgi-bin/login.php
@@ -1,15 +1,44 @@
-<!-- <html>
-<body> -->
 
-<?php
-echo "Data processed" . "<br>";
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+        <link rel="stylesheet" type="text/css" href="../css/general.css">
+        <link rel="stylesheet" type="text/css" href="../css/room.css">
+        <script src="../src/login.js"></script>
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+</head>
+<body>
 
-echo "Username : " . $username . " <br>";
+    <div id="navbar">
+        <a href="home.html">Home</a>
+        <a href="leaderboard.html">Leaderboard</a>
+        <a href="FAQ.html">FAQ</a>
+        <a href="about.html">About</a>
+        <img src="../images/logo.png" height="45" style="float: right" />    
+    </div>
 
-?>
+  <form action = "../cgi-bin/login.php" method="POST" id ="login-form">
 
-<!-- </body>
-</html> -->
+    <div id="login">
+        <h1 id = "login-text">Login</h1>
+        <br>
+        <label class="input">
+            <input type="text" id="username" placeholder="Username"/>
+        </label>
+
+        <br><br>    
+
+        <label class="input">
+            <input type="password" id="password" placeholder="Password"/>
+        </label>
+
+        <br><br>
+        <input type="submit" value ="Login" name = "submit" class = "general-button">
+    </div>
+
+  </form>
+</body>
+</html>
