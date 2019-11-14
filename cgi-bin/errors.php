64a768710b03e490b45dec9f8a73784500dddcbe
Abhishek262 <abhishek262014@gmail.com>
2019-11-14 10:52:06 +0530

    error message css

--- cgi-bin/errors.php
+++ cgi-bin/errors.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
   <div class="error">
   	<?php foreach ($errors as $error) : ?>
-  	  <p class = 'txt-white'><?php echo $error; ?></p>
+  	  <div class = 'general-error'><?php echo $error; ?></div>
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
--- cgi-bin/login.php
+++ cgi-bin/login.php
@@ -47,15 +47,15 @@
             <div class="lbl">
                 <label class="txt-white">Username</label>
                 <br>
-                <input type="text" id="username" class="tb" placeholder="username" name = "username">
+                <input type="text" id="username" class="tb" placeholder="Username" name = "username">
             </div>
-            <br>
+            <br><br>
             <div class="lbl">
                 <label class="txt-white">Password</label>
                 <br>
                 <input type="password" id="password" name="password" class="tb" placeholder="Password" class="general-text">
             </div>
-            <br>
+            <br><br>
             <input type="submit" value="Login" name="login" class="general-button">
         </div>
 
@@ -63,6 +63,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
