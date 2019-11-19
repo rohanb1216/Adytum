eda6c119a0d0c2016dab9dc8b92188ac069e801d
Abhishek262 <abhishek262014@gmail.com>
2019-11-19 23:03:19 +0530

    Fixed get started button redirection

--- src/home.js
+++ src/home.js
@@ -1,3 +1,16 @@
 function play(){
     window.location.href="../views/"+room.PlayerRoom+".html";
 }
+
+function redirect(){
+    var l = document.getElementById("login-logout-button");
+    if(l.value == " Login "){
+        window.location.href="../cgi-bin/login.php";
+    }
+    else{
+        play();
+    }
+}
+
+var x = document.getElementById("get-started");
+x.addEventListener("click",redirect);
