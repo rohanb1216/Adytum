a0ce5c654eb02a4b3da53a42a92177c4c83ef980
Abhishek262 <abhishek262014@gmail.com>
2019-11-21 20:52:15 +0530

    map.js bug fix

--- src/map.js
+++ src/map.js
@@ -4,8 +4,9 @@ for(let i=0;i< map.childElementCount;i++){
     block_arr.push(map.children[i]);
 }
 
-currRoom = Number(room.PlayerRoom.replace('room',''));
-console.log(currRoom);
+var room_no = room.PlayerRoom;
+room_no = Number(room_no.replace('room',''));
+console.log(room_no);
 
 
 room_arr = [room.room1 ,room.room2,room.room3,room.room4,room.room5,room.room6,room.room7,room.room8,room.room9];
@@ -15,5 +16,5 @@ for(let i =0;i< block_arr.length;i++){
         block_arr[i].classList.toggle("open-block");
     }
 }
-block_arr[currRoom-1].classList.remove("open-block");
-block_arr[currRoom-1].classList.toggle("curr-block");
+block_arr[room_no-1].classList.remove("open-block");
+block_arr[room_no-1].classList.toggle("curr-block");
