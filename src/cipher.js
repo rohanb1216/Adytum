30369eb4be9062d2f71f8c16ecb90aa3bd519f3a
rohanb1216 <rohan.biju@gmail.com>
2019-11-22 12:42:06 +0530

    almost final

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,4 @@
-var question = "adytum";
-
+question = hashCode(question);
 function cipher(text) {
     var len = text.length;
     for (var i = 0; i < len; i++) {
@@ -7,34 +6,38 @@ function cipher(text) {
     }
 }
 
-function caesarCipher(str, key) {
-    return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65));
-}
+// function caesarCipher(str, key) {
+//     return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0) - 65 + key) % 26 + 65));
+// }
 
 function showText(text) {
     document.getElementById("question").innerHTML = text;
 }
+function hashCode(s) {
+    return s.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
+}
 
 function check() {
-    var answer = document.getElementById("answer").value;
+    var answer = hashCode(document.getElementById("answer").value);
+
     console.log(currRoom);
     if (answer == question) {
         solved(currRoom);
-        // alert("solved");
-        document.getElementById("incorrect").value="Correct";
+        alert("solved");
+        document.getElementById("incorrect").value = "Correct";
         document.getElementById("answer").classList.remove(".incorrect");
         hide("puzzle");
     }
-    else{
+    else {
         alert("incorrect");
         console.log(question);
         document.getElementById("answer").classList.add(".incorrect");
     }
 }
 
-window.onload = function() {
-    var text = caesarCipher(question, Math.floor(Math.random() * 26));
-    showText(text);
-    //document.getElementsByTagName("button")[0].addEventListener("click", check);
+// window.onload = function() {
+//     var text = caesarCipher(question, Math.floor(Math.random() * 26));
+//     showText(text);
+//     //document.getElementsByTagName("button")[0].addEventListener("click", check);
 
-};
+// };
